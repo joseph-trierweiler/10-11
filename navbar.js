8f873c5625c3a2e2dccb89a8e0262d7c6a6505a8
michael.greene2 <michael.greene2@ey.com>
2022-08-10 15:56:36 -0500

    updates to navbar

--- navbar.js
+++ navbar.js
@@ -1,8 +1,24 @@
 const menuBtn = document.querySelector(".nav-menu-icon");
-const dropdown = document.querySelector("#nav-links-container-dropdown");
 let menuOpen = false;
+const dropdownContainer = document.getElementById("dropdown-container");
+
+const dropdownHtml = `<span id="nav-links-container-dropdown"> 
+<div><a class="nav-links" href="">Menu</a></div>
+<div><a class="nav-links" href="">Rewards</a></div>
+<div><a class="nav-links" href="">Locations</a></div>
+<div><a class="nav-links" href="">Careers</a></div>
+<div><a class="nav-links" href="">About Us</a></div>
+<hr id="dropdown-line" />
+<div><a class="nav-links" href="">Login</a></div>
+<div><a class="nav-links" href="">Sign Up</a></div>
+<div class="nav-links">&nbsp;</div>
+<div><a id="nav-order-btn-dropdown" href="">Order Now</a></div>
+</span>`;
 
 menuBtn.addEventListener("click", () => {
+  dropdownContainer.innerHTML = dropdownHtml;
+  const dropdown = document.querySelector("#nav-links-container-dropdown");
+
   if (!menuOpen) {
     menuBtn.classList.add("open");
     menuOpen = true;
