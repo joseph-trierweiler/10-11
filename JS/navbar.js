6fcc146a60a5ad475267da0a417dc55a778dea63
darrielle.evans <darrielle.evans@ey.com>
2022-08-12 10:49:33 -0500

    merge

--- JS/navbar.js
+++ JS/navbar.js
@@ -3,8 +3,8 @@ let menuOpen = false;
 const dropdownContainer = document.getElementById("dropdown-container");
 
 const dropdownHtml = `<span id="nav-links-container-dropdown"> 
-<div><a class="nav-links" href="">Menu</a></div>
-<div><a class="nav-links" href="">Rewards</a></div>
+<div><a class="nav-links" href="index.html">Menu</a></div>
+<div><a class="nav-links" href="Html/rewards.html">Rewards</a></div>
 <div><a class="nav-links" href="">Locations</a></div>
 <div><a class="nav-links" href="">Careers</a></div>
 <div><a class="nav-links" href="">About Us</a></div>
@@ -29,3 +29,21 @@ menuBtn.addEventListener("click", () => {
     dropdown.style.display = "none";
   }
 });
+
+/* Navbar Highlighting
+const currentWindow = location.href;
+const navItem = document.querySelectorAll("a");
+const navItemLength = navItem.length;
+for (let i = 0; i < navItemLength; i++) {
+  if (navItem[i].href === currentWindow) {
+    navItem[i].className = "active";
+  }
+}*/
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll("a");
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+  if (menuItem[i].href === currentLocation) {
+    menuItem[i].className = "active";
+  }
+}
