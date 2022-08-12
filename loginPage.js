c275f2c3d9481a0390882c685ab4067573888ebb
DAndre.Turner <DAndre.Turner@ey.com>
2022-08-12 12:25:49 -0400

    jsNameUpdate

--- loginPage.js
+++ loginPage.js
@@ -1,20 +1,13 @@
 
 
-// grab order now button from DOM
-
 const loginModal = document.getElementById("logInPage");
 
-// grab the order now modal container from DOM
 
 const loginContainerModal = document.getElementById("loginModal_container");
 
-// grab close button (x) from DOM
 
 const closeLoginModal = document.getElementById("closeModalBtn");
 
-
-
-// add event listener to order now button
 
 loginModal.addEventListener('click', () => {
 
@@ -22,9 +15,6 @@ loginModal.addEventListener('click', () => {
 
 });
 
-
-
-// add event event listener to close (x) button
 
 closeLoginModal.addEventListener('click', () => {
 
