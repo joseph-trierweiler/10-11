6bb541b0725c89f3069ef1137aa4ebfb69910c09
darrielle.evans <darrielle.evans@ey.com>
2022-08-12 15:18:44 -0500

    fixed hamburger menu links

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,24 +1,9 @@
 const menuBtn = document.querySelector(".nav-menu-icon");
 let menuOpen = false;
 const dropdownContainer = document.getElementById("dropdown-container");
-
-const dropdownHtml = `<span id="nav-links-container-dropdown"> 
-<div><a class="nav-links" href="index.html">Menu</a></div>
-<div><a class="nav-links" href="Html/rewards.html">Rewards</a></div>
-<div><a class="nav-links" href="">Locations</a></div>
-<div><a class="nav-links" href="">Careers</a></div>
-<div><a class="nav-links" href="">About Us</a></div>
-<hr id="dropdown-line" />
-<div><a class="nav-links" href="">Login</a></div>
-<div><a class="nav-links" href="">Sign Up</a></div>
-<div class="nav-links">&nbsp;</div>
-<div><a id="nav-order-btn-dropdown" href="">Order Now</a></div>
-</span>`;
+const dropdown = document.querySelector("#nav-links-container-dropdown");
 
 menuBtn.addEventListener("click", () => {
-  dropdownContainer.innerHTML = dropdownHtml;
-  const dropdown = document.querySelector("#nav-links-container-dropdown");
-
   if (!menuOpen) {
     menuBtn.classList.add("open");
     menuOpen = true;
@@ -30,15 +15,6 @@ menuBtn.addEventListener("click", () => {
   }
 });
 
-/* Navbar Highlighting
-const currentWindow = location.href;
-const navItem = document.querySelectorAll("a");
-const navItemLength = navItem.length;
-for (let i = 0; i < navItemLength; i++) {
-  if (navItem[i].href === currentWindow) {
-    navItem[i].className = "active";
-  }
-}*/
 const currentLocation = location.href;
 const menuItem = document.querySelectorAll("a");
 const menuLength = menuItem.length;
@@ -47,3 +23,20 @@ for (let i = 0; i < menuLength; i++) {
     menuItem[i].className = "active";
   }
 }
+
+// modal
+
+const OpenOrderModal = document.getElementById("OrderNowBtn");
+const OrderModal = document.getElementById("OrderModalContainer");
+const CloseOrderModal = document.getElementById("CloseOrderBtn");
+
+OpenOrderModal.addEventListener("click", () => {
+  OrderModal.classList.add("showOrderNowModal");
+  console.log("hey");
+});
+
+CloseOrderModal.addEventListener("click", () => {
+  OrderModal.classList.remove("showOrderNowModal");
+});
+
+// login JS
