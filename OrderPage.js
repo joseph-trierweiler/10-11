06af15729204779f35a5c30a9733a9b4949b27de
charmaine.stegall <charmaine.stegall@ey.com>
2022-08-11 20:17:37 -0400

    added JS file and completed modal update

---
+++ OrderPage.js
@@ -0,0 +1,12 @@
+console.log("Is this thing on??")
+
+var orderNowBtn = document.getElementById("OrderNow");
+
+let showModal = () => {
+     document.getElementById('modalContainer').classList.add("showOrderNowModal");
+}
+ let hideModal = () => {
+    document.getElementById('modalContainer').classList.remove("showOrderNowModal");
+ }
+
+
