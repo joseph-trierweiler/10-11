c54123bec09839ad9d8ecc483ec0889e70f7244a
charmaine.stegall <charmaine.stegall@ey.com>
2022-08-24 15:55:44 -0400

    changed CSS to hypen casing

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,14 +4,14 @@ import './Card.css';
 function Card({sectionTitle, src, alt, title, description, price}) {
     return (
 
-            <div className="Card">
-                    <img src={src} alt={alt} className="CardImg"></img>
-                <div className="CardBox">
-                    <h3 className="CardTitle">{title}</h3>
-                    <div className="CardDescription">
+            <div className="card">
+                    <img src={src} alt={alt} className="card-img"></img>
+                <div className="card-box">
+                    <h3 className="card-title">{title}</h3>
+                    <div className="card-description">
                         {description}
                     </div>
-                    <h4 className="CardPrice">{price}</h4>
+                    <h4 className="card-price">{price}</h4>
                 </div>
             </div>
 
