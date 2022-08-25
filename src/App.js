ce0645c67d646684d1984604147b4bfe52f41acd
michael.greene2 <michael.greene2@ey.com>
2022-08-25 13:32:11 -0500

    cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import React, { useState } from 'react';
-import LoginSignUpModal from './components/login/LoginSignUpModal';
 import './App.css';
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const modalHandler = () => {
-    setIsModalOpen(true);
-  };
-
   return <div className="App">hey</div>;
 }
 
