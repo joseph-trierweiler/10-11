b99c67160f4b1888e7f1ebca758c1e4a49303f17
charmaine.stegall <charmaine.stegall@ey.com>
2022-08-26 11:04:58 -0400

    added rewards page js and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import RewardsPage from './components/rewards/rewards';
 
 
 function App() {
   return (
     <div className="App">
-        hey
+        <RewardsPage/>
     </div>
   );
 }
---
+++ src/components/rewards/rewards.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './rewards.module.css';
+import NoodleImg from '../../assets/images/Noodles.png';
+
+const RewardsPage = () => {
+    return(
+        <div className={styles.rewardsContainer}>
+            <div className={styles.rewardsTextContainer}>
+                <div className={styles.rewardsText}>
+                    <p className={styles.rewardsTitle}>Noodles Rewards</p>
+                    <h1 className={styles.rewardsHeading }>The fastest way to 
+                    free noodles </h1>
+                    <p className='descriptionText'>Earn points ordering your faves and cash them out
+                        for a variety of rewards in the Rewards Exchange.
+                        Check out your Extras for ways to unlock extra
+                        points and collect exclusive badges. Not a
+                        member yet? Join to start earning.
+                    </p>
+                    <div className='descriptionText2'>
+                        Program Terms
+                    </div>
+                </div>
+            </div>
+            <div className={styles.rewardsImgContainer}>
+                <img className={styles.rewardsImg} src={NoodleImg} alt='Bowl of noodles'/>
+            </div>
+        </div>
+    )
+}
+
+export default RewardsPage
