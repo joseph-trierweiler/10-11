7715d4a72d1b511c8d22f54f4d24b94bea9b78a1
charmaine.stegall <charmaine.stegall@ey.com>
2022-08-26 18:07:39 -0400

    created object for rewards info and completed unit testing

--- src/components/rewards/rewards.jsx
+++ src/components/rewards/rewards.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import styles from './rewards.module.css';
 import NoodleImg from '../../assets/images/Noodles.png';
 
-const RewardsPage = () => {
+
+
+const RewardsPage = ( ) => {
+    const RewardsInfo = {
+        title: 'NOODLES REWARDS',
+        heading: 'The fastest way to free noodles',
+        description: 'Earn points ordering your faves and cash them out for a variety of rewards in the Rewards Exchange. Check out your Extras for ways to unlock extra points and collect exclusive badges. Not a member yet? Join to start earning.',
+        terms: 'Program Terms'
+    }
     return(
-        <div className={styles.rewardsContainer}>
+        <div className={styles.rewardsContainer} data-testid='rewardsContainer'>
             <div className={styles.rewardsTextContainer}>
                 <div className={styles.rewardsText}>
-                    <p className={styles.rewardsTitle}>NOODLES REWARDS</p>
-                    <h1 className={styles.rewardsHeading }>The fastest way to 
-                    free noodles </h1>
-                    <p className={styles.rewardsDescription}>Earn points ordering your faves and cash them out
-                        for a variety of rewards in the Rewards Exchange.
-                        Check out your Extras for ways to unlock extra
-                        points and collect exclusive badges. Not a
-                        member yet? Join to start earning.
+                    <p className={styles.rewardsTitle}>{RewardsInfo.title}</p>
+                    <h1 className={styles.rewardsHeading }>{RewardsInfo.heading} </h1>
+                    <p className={styles.rewardsDescription}>{RewardsInfo.description}
                     </p>
                     <div className={styles.rewardsTerms}>
                         Program Terms
