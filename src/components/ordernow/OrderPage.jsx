84436c137e4f3a50c151db7fc4674c47a9e583f5
michael.greene2 <michael.greene2@ey.com>
2022-08-29 14:20:12 -0500

    add close out button to Order now

--- src/components/ordernow/OrderPage.jsx
+++ src/components/ordernow/OrderPage.jsx
@@ -1,22 +1,24 @@
-import styles from "./ordernow.module.css";
-import Noodles from "../../assets/images/background_noodle.png";
-import React, { useState } from "react";
+import styles from './ordernow.module.css';
+import Noodles from '../../assets/images/background_noodle.png';
+import React, { useState } from 'react';
 
-const OrderPage = () => {
-  const [label, setLabel] = useState("Pickup at a location near you");
-  const [input, setInput] = useState("Enter City, State, or Zip");
+const OrderPage = (props) => {
+  const [label, setLabel] = useState('Pickup at a location near you');
+  const [input, setInput] = useState('Enter City, State, or Zip');
+  const closeModal = () => props.isModalClosed(false);
 
   return (
     <div className={styles.orderModal}>
       <img alt="this is a pic" src={Noodles} className={styles.panel} />
+
       <div className={styles.panel}>
         <div className={styles.buttonSection}>
           <div>
             <button
               className={styles.pickup}
               onClick={() => {
-                setLabel("Pickup at a location near you:");
-                setInput("Enter City, State, or Zip");
+                setLabel('Pickup at a location near you:');
+                setInput('Enter City, State, or Zip');
               }}
             >
               Pickup
@@ -25,8 +27,8 @@ const OrderPage = () => {
             <button
               className={styles.delivery}
               onClick={() => {
-                setLabel("Please type your address:");
-                setInput("Enter Your Address");
+                setLabel('Please type your address:');
+                setInput('Enter Your Address');
               }}
             >
               Delivery
@@ -45,13 +47,17 @@ const OrderPage = () => {
             <br />
             <button
               className={styles.useMyLocation}
-              onClick={() => setInput("401 9th Ave, New York, NY 10001")}
+              onClick={() => setInput('401 9th Ave, New York, NY 10001')}
             >
-              {" "}
+              {' '}
               📍 Use My Location
             </button>
           </div>
         </div>
+      </div>
+      <div className={styles.closeModalBtn} onClick={closeModal}>
+        {' '}
+        &times;
       </div>
     </div>
   );
